61a2d3b390e910dad51387fdd66016f901704277
macbook <macbook@macbooks-MacBook-Air.local>
2019-08-27 20:03:15 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,20 @@
-import { fetchJobs } from "..data/jobs.js";
-fetchJobs ('Sales_Executive');
+// import  {fetchJobs} from "..data/jobs.js";
+// fetchJobs ('Sales_Executive');
 
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const jobs = fetchjobs;
-  res.render('jobboard', { title: 'jobgrabba',jobs: jobs });
+  res.render('jobboard', { title: 'jobgrabba',jobs: 'jobs' });
 });
 
 router.get('/views/fetchjobs', function(req, res, next){
   res.render('fetchjobs', {title: 'jobgrabba'});
 });
 
-router.get('/views/jobs.js'), function(req, res, next){
+router.get('/views/jobs' , function(req, res, next){
   res.render('jobs', {title: 'jobs'});
-}
+});
 
 module.exports = router;
